21a0e130de3bdc6997bc921b943618acd6585afd
Giga <60932604+Gkhundadze@users.noreply.github.com>
2022-02-03 15:08:35 +0400

    fixed console error
    code dropped inside if statement so it depends on page path
    window.location.pathname

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let alphabet = ['ა','ბ','გ','დ','ე','ვ','ზ','თ','ი','კ','ლ','მ','ნ','ო','პ','ჟ','რ','ს','ტ','უ','ფ','ქ','ღ','ყ','შ','ჩ','ც','ძ','წ','ჭ','ხ','ჯ','ჰ'];
+if(document.location.pathname == '/dictionary.html'){
+    let alphabet = ['ა','ბ','გ','დ','ე','ვ','ზ','თ','ი','კ','ლ','მ','ნ','ო','პ','ჟ','რ','ს','ტ','უ','ფ','ქ','ღ','ყ','შ','ჩ','ც','ძ','წ','ჭ','ხ','ჯ','ჰ'];
 alphabet.forEach((letter)=> {
     let child = document.createElement('span');
     child.innerText = letter;
@@ -8,3 +9,5 @@ let lchange = document.createElement('span');
 lchange.className = 'lchange';
 lchange.innerText = 'ENG'
 document.querySelector('.alphabet-wrapper').appendChild(lchange)
+}
+
